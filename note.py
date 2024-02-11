55b7fb71e4f30725b88096365566fe908e6e46ca
Tomiomgg <odeletomi@gmail.com>
2024-02-11 17:22:28 -0600

    another python porject

---
+++ note.py
@@ -0,0 +1,20 @@
+numbers = ["Tomi", "Ayo", "Odele", "Penial"]
+for x in numbers:
+    print (x)
+
+colon = ( 1, 98, 64, 9033)
+for cordinate in colon:
+    print (cordinate)
+
+name = "Tomilola"
+for char in name:
+    print (char)
+
+character = {
+    "gender" : "girl",
+    "first_name" : "tomi",
+    "last_name" : "odele",
+    "age" : 13
+}
+for key,value in character.items():
+    print (f"{key} : {value}")
